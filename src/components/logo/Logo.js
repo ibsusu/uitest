6148492cacce5e01787a6a420ba4507c7a0ea9fd
大北 <@gmail.com>
2022-10-22 14:15:57 -0700

    logo fix

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -2,20 +2,11 @@ import PropTypes from 'prop-types';
 import { forwardRef } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 // @mui
-import { useTheme } from '@mui/material/styles';
 import { Box, Link } from '@mui/material';
 
 
 
 const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
-  const theme = useTheme();
-
-  const PRIMARY_LIGHT = theme.palette.primary.light;
-
-  const PRIMARY_MAIN = theme.palette.primary.main;
-
-  const PRIMARY_DARK = theme.palette.primary.dark;
-
   const logo = (
     <Box
       ref={ref}
